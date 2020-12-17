4fa96c93930a0b80c4286bef991f396c08bcfffc
bcn-alumno <bcn-alumno@nm_madrid.local>
2020-12-17 10:27:12 +0100

    Exercice done

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 // Tu código aquí
 
-const form = document.querySelector('#message-form')
+const form = document.querySelector("#message-form");
 
-form.addEventListener('submit', function(e){
-    console.log("Información sobre el objeto event: ", e)
-    e.preventDefault() // Curioso sobre lo que hace esto? Mira como se comporta el formulario cuando haces clic en "Enviar" si esta línea está comentada. 
+form.addEventListener("submit", function (e) {
+  console.log("Información sobre el objeto event: ", e);
+  e.preventDefault(); // Curioso sobre lo que hace esto? Mira como se comporta el formulario cuando haces clic en "Enviar" si esta línea está comentada.
 
-    console.log("Hemos hecho clic en el botón de submit!")
+  let palabra = document.querySelector("#message");
+  let campoPalabra = document.querySelector(".message-content");
 
-})
+  campoPalabra.innerHTML = palabra.value;
+
+  console.log("Hemos hecho clic en el botón de submit!");
+});
