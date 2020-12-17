7fab9aace6acf773219642cf547d0a027bd2198d
bcn-alumno <bcn-alumno@nm_madrid.local>
2020-12-17 11:44:25 +0100

    Add bonus part, error message if emnpty submit

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 // Tu código aquí
 
 const form = document.querySelector("#message-form");
+let errorAlert = document.querySelector("#error");
 
 form.addEventListener("submit", function (e) {
   console.log("Información sobre el objeto event: ", e);
@@ -11,10 +12,17 @@ form.addEventListener("submit", function (e) {
   //We select the camp where the word will get printed and add it on a variable
   let campoPalabra = document.querySelector(".message-content");
 
-  //And we just simply print into campPalabra, the value input by the user on the input field.
-  campoPalabra.innerHTML = palabra.value;
+  //First we check if the value is empty or not, and if it's empty we make the error message appear and get printed.
+  if (!palabra.value) {
+    errorAlert.style.display = "block";
+    errorAlert.innerHTML = "Error! You didn't type any word!";
+  }
+  // But if it has some value, just prints the value on the camp we selected earlier
+  else {
+    campoPalabra.innerHTML = palabra.value;
+  }
 
   //And an extra touch, with this line we just clean the input field
   palabra.value = "";
-  console.log("Hemos hecho clic en el botón de submit!");
+  // console.log("Hemos hecho clic en el botón de submit!");
 });
